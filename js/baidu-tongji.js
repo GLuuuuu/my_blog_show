92fc689a50c4fea44f14c7e166233a8e8f76211e
lu <suus9809@gmail.com>
2026-04-24 05:40:52 +0000

    Sync public folder from main repository

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,16 @@
-var _hmt = _hmt || [];
+// {<script>
+// var _hmt = _hmt || [];
+// (function() {
+//   var hm = document.createElement("script");
+//   hm.src = "https://hm.baidu.com/hm.js?20a51711d2daabcd07f898d46140a059";
+//   var s = document.getElementsByTagName("script")[0]; 
+//   s.parentNode.insertBefore(hm, s);
+// })();
+// </script> }
+let _hmt = _hmt || [];
 (function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
-  var s = document.getElementsByTagName("script")[0]; 
+  let hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?20a51711d2daabcd07f898d46140a059";
+  let s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
